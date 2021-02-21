05585e890064ff0b05b3df64391191a6e0557f80
joomoraes <52724220+joomoraes@users.noreply.github.com>
2021-02-21 16:08:00 -0300

    Começando o projeto

---
+++ src/main/java/com/cloud/mc/APiMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APiMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APiMongoDbApplication.class, args);
+	}
+
+}
