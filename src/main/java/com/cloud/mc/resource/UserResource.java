084343fbd53c92f4d58aad7363eb7e0b07993bf8
joomoraes <52724220+joomoraes@users.noreply.github.com>
2021-02-22 20:46:08 -0300

    Atribuição do POST ao algoritmo

--- src/main/java/com/cloud/mc/resource/UserResource.java
+++ src/main/java/com/cloud/mc/resource/UserResource.java
@@ -1,14 +1,17 @@
 package com.cloud.mc.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cloud.mc.DTO.UserDTO;
 import com.cloud.mc.domain.User;
@@ -33,4 +36,15 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
 }
--- src/main/java/com/cloud/mc/services/UserServices.java
+++ src/main/java/com/cloud/mc/services/UserServices.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cloud.mc.DTO.UserDTO;
 import com.cloud.mc.domain.User;
 import com.cloud.mc.repository.UserRepository;
 
@@ -24,5 +25,13 @@ public class UserServices {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user;
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
