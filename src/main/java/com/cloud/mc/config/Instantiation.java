e4edb34f664fc55769566c93091179e7402750d6
joomoraes <52724220+joomoraes@users.noreply.github.com>
2021-02-22 18:07:20 -0300

    Operação de inserção da base de dados

---
+++ src/main/java/com/cloud/mc/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.cloud.mc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.cloud.mc.domain.User;
+import com.cloud.mc.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override 
+	public void run(String... arg0) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		// userRepository.save(Arrays.asList(maria, alex, bob));
+	}
+}
