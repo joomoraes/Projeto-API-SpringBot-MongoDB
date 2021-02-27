5c5772873f1c9068ff3879cc2dac50dade976837
joomoraes <52724220+joomoraes@users.noreply.github.com>
2021-02-27 16:32:03 -0300

    Consulta simples com query

--- src/main/java/com/cloud/mc/repository/PostRepository.java
+++ src/main/java/com/cloud/mc/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.cloud.mc.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cloud.mc.domain.Post;
@@ -11,6 +12,9 @@ import com.cloud.mc.domain.User;
 @Repository
 public interface PostRepository extends MongoRepository<User, String>{
 
-	List<Post> findByTitleContaing(String text);
+	@Query(" <title>: { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	List<Post> findByTitleContaingIgnoreCase(String text);
 	
 }
